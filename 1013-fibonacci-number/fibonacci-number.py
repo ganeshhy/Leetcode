419f2bf83de8746e068f953160d9b62153c64762
Ganesh H Y <ganeshhy1903@gmail.com>
2024-12-31 23:36:01 +0530

    Time: 15 ms (68.79%) | Memory: 12.4 MB (16.68%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==0:
+            return 0
+        elif n==1:
+            return 1
+        a,b=0,1
+        for i in range(1,n):
+            a,b=b,a+b
+        return b
+        
